7373cee533a0034bd78fcbd296b4107d4d335027
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-09 15:35:09 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -39,13 +39,8 @@ def hit?(card_total)
   # code hit? here
   prompt_user
   get_user_input
- if get_user_input == "h"
-    card_total += deal_card
-  elsif get_user_input == "s"
-  card_total
-else
-    invalid_command
-  end
+ if get_user_input == "s"
+   card_total
   
   
 end
